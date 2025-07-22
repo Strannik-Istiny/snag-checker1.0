9c3fd13f15057083f0a1c5440451a38bf26ac132
Strannik-Istiny <seregga2578@gmail.com>
2025-07-23 01:00:53 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';   // файл пока не обязателен, но строку оставим
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
