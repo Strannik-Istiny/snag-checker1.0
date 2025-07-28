c0e5f46da1b6f032205d6743309c69f09527f471
Strannik-Istiny <seregga2578@gmail.com>
2025-07-28 17:08:07 +0300

    Update update-salaries.cli.js

--- functions/update-salaries.cli.js
+++ functions/update-salaries.cli.js
@@ -1,13 +1,6 @@
 import fs from 'fs';
-const salaries = {
-  RU: 87952,
-  BY: 1800,
-  KZ: 350542,
-  UA: 18589,
-  AM: 310000,
-  AZ: 900,
-  KG: 23939,
-  UZ: 5360000,
+const s = {
+  RU: 87952, BY: 1800, KZ: 350542, UA: 18589,
+  AM: 310000, AZ: 900, KG: 23939, UZ: 5360000
 };
-fs.writeFileSync('./src/salaries.json', JSON.stringify(salaries, null, 2));
-console.log('salaries.json updated');
+fs.writeFileSync('./src/salaries.json', JSON.stringify(s, null, 2));
