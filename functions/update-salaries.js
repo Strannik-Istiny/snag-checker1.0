614083bb97b70f80fed0eb0f7ea17f06ed5663b7
Strannik-Istiny <seregga2578@gmail.com>
2025-08-06 14:38:42 +0300

    Delete functions/update-salaries.js

--- functions/update-salaries.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-const s = {
-  RU: 87952, BY: 1800, KZ: 350542, UA: 18589,
-  AM: 310000, AZ: 900, KG: 23939, UZ: 5360000
-};
-fs.writeFileSync('./src/salaries.json', JSON.stringify(s, null, 2));
